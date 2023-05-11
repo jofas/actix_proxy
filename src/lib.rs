723e71b29fdbbc5b6b78ffe840be4e25e9fb6db1
jofas <jonas@fc-web.de>
2023-05-11 10:30:07 +0200

    default format

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,7 @@
 use actix_web::http::StatusCode;
 use actix_web::{dev, HttpResponse, ResponseError};
 
-use awc::error::{
-  ConnectError, SendRequestError as AwcSendRequestError,
-};
+use awc::error::{ConnectError, SendRequestError as AwcSendRequestError};
 use awc::ClientResponse;
 
 use std::fmt;
@@ -20,29 +18,27 @@ use std::fmt;
 pub struct SendRequestError(AwcSendRequestError);
 
 impl fmt::Display for SendRequestError {
-  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    write!(f, "{}", self.0)
-  }
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
 }
 
 impl From<AwcSendRequestError> for SendRequestError {
-  fn from(e: AwcSendRequestError) -> Self {
-    Self(e)
-  }
+    fn from(e: AwcSendRequestError) -> Self {
+        Self(e)
+    }
 }
 
 /// Convert `SendRequestError` to a server `Response`
 ///
 impl ResponseError for SendRequestError {
-  fn status_code(&self) -> StatusCode {
-    match self.0 {
-      AwcSendRequestError::Connect(ConnectError::Timeout) => {
-        StatusCode::GATEWAY_TIMEOUT
-      }
-      AwcSendRequestError::Connect(_) => StatusCode::BAD_REQUEST,
-      _ => StatusCode::INTERNAL_SERVER_ERROR,
+    fn status_code(&self) -> StatusCode {
+        match self.0 {
+            AwcSendRequestError::Connect(ConnectError::Timeout) => StatusCode::GATEWAY_TIMEOUT,
+            AwcSendRequestError::Connect(_) => StatusCode::BAD_REQUEST,
+            _ => StatusCode::INTERNAL_SERVER_ERROR,
+        }
     }
-  }
 }
 
 /// Trait for converting a [`ClientResponse`] into a [`HttpResponse`].
@@ -55,42 +51,40 @@ impl ResponseError for SendRequestError {
 /// [`HttpResponse`]: HttpResponse
 ///
 pub trait IntoHttpResponse {
-  /// Creates a [`HttpResponse`] from `self`.
-  ///
-  /// [`HttpResponse`]: HttpResponse
-  ///
-  fn into_http_response(self) -> HttpResponse;
+    /// Creates a [`HttpResponse`] from `self`.
+    ///
+    /// [`HttpResponse`]: HttpResponse
+    ///
+    fn into_http_response(self) -> HttpResponse;
 
-  /// Wraps the [`HttpResponse`] created by [`into_http_response`]
-  /// in a `Result`.
-  ///
-  /// # Errors
-  ///
-  /// Because [`into_http_response`] is infallible, this method is,
-  /// too.
-  /// So calling this method never fails and never returns an `Err`.
-  ///
-  /// [`HttpResponse`]: HttpResponse
-  /// [`into_http_response`]: Self::into_http_response
-  ///
-  fn into_wrapped_http_response<E>(self) -> Result<HttpResponse, E>
-  where
-    Self: Sized,
-  {
-    Ok(self.into_http_response())
-  }
+    /// Wraps the [`HttpResponse`] created by [`into_http_response`]
+    /// in a `Result`.
+    ///
+    /// # Errors
+    ///
+    /// Because [`into_http_response`] is infallible, this method is,
+    /// too.
+    /// So calling this method never fails and never returns an `Err`.
+    ///
+    /// [`HttpResponse`]: HttpResponse
+    /// [`into_http_response`]: Self::into_http_response
+    ///
+    fn into_wrapped_http_response<E>(self) -> Result<HttpResponse, E>
+    where
+        Self: Sized,
+    {
+        Ok(self.into_http_response())
+    }
 }
 
-impl IntoHttpResponse
-  for ClientResponse<dev::Decompress<dev::Payload>>
-{
-  fn into_http_response(self) -> HttpResponse {
-    let mut response = HttpResponse::build(self.status());
+impl IntoHttpResponse for ClientResponse<dev::Decompress<dev::Payload>> {
+    fn into_http_response(self) -> HttpResponse {
+        let mut response = HttpResponse::build(self.status());
 
-    self.headers().into_iter().for_each(|(k, v)| {
-      response.insert_header((k, v));
-    });
+        self.headers().into_iter().for_each(|(k, v)| {
+            response.insert_header((k, v));
+        });
 
-    response.streaming(self)
-  }
+        response.streaming(self)
+    }
 }
